eda2834030b98db53e6081014dba5c43ad58e3d1
JulienGrade <gradejulien@gmail.com>
2023-02-07 11:30:41 +0100

    Add products for homepage

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomepageController extends Controller
 {
@@ -15,8 +17,8 @@ class HomepageController extends Controller
      */
     public function index()
     {
-        $name = "John";
+        $products = Product::orderBy('product_price', 'desc')->paginate(1);
 
-        return view('homepage.home', ['name' => $name]);
+        return view('homepage.home')->with('products', $products);
     }
 }
--- resources/views/homepage/home.blade.php
+++ resources/views/homepage/home.blade.php
@@ -5,6 +5,20 @@
 
 @section('content')
     <div class="card mt-5 p-5 bg-primary text-white">
-        <h1>Welcome {{$name}} to the Laravel Project</h1>
+        <h1>Welcome to the Laravel Project</h1>
     </div>
+    @foreach($products as $product)
+        <div class="card w-50 mx-auto my-5 text-dark">
+            <div class="card-header">
+                <h1 class="text-center">{{$product->product_name}}</h1>
+            </div>
+            <div class="card-body">
+                <p class="text-center fs-3">{{$product->product_description}}</p>
+            </div>
+            <div class="card-footer">
+                <p class="text-end text-primary text-bolder fs-2 fw-bold">{{$product->product_price}} €</p>
+            </div>
+        </div>
+    @endforeach
+    {{$products->links()}}
 @endsection
